eb0e531d794d9e2d5374baf2b8015f9a07c202f8
dedev5 <dedev5430@gmail.com>
2022-12-03 17:26:01 -0500

    Added hellocontroller

---
+++ hello-controller.js
@@ -0,0 +1,12 @@
+const HelloController = (app) => {
+    app.get('/hello', (req, res) => {
+        res.send('Life is good!')
+    });
+    app.get('/goodbye', (req, res) => {
+        res.send('Life is good! Or is it?')
+    });
+    app.get('/', (req, res) => {
+        res.send('Welcome to Full Stack Development!')
+    });
+}
+export default HelloController;
